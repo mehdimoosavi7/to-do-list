d9791a93910a00870ef75a79cce2bb3137b2b58a
Parsa s <51279606+parsa-eng01@users.noreply.github.com>
2023-08-26 11:32:30 +0330

    update: use props to show/hide editbox

--- src/components/EditBox.js
+++ src/components/EditBox.js
@@ -1,6 +1,6 @@
 export default function Editbox(props) {
     return (
-        <div className="editBox" id="editBox">
+        <div className={(props.isVisible ? "show " : "") + "editBox"} id="editBox">
             <input type="text" id="editTextBox" placeholder={props.text} />
             <button type="button" className="add-btn" id="addButton" onClick={props.Func}>{props.action}</button>
             <button type="button" className="cancel-btn" id="cancelButton" onClick={hideEditBox}>Cancel</button>
