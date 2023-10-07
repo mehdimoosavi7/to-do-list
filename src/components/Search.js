df3d764a5766960f2469a4e1f300b77cafb7cfbf
mehdimoosavi7 <mehdimoosavi7@gmail.com>
2023-10-07 13:28:32 +0330

    Updates

--- src/components/Search.js
+++
@@ -1,6 +0,0 @@
-import React  from 'react';
-export default function Search(props) {
-    return(
-        <input type="text" className='searchText' placeholder="Search tasks..." onChange={props.func} value={props.value}/>
-    )
-}
